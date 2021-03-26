f66b03d3c6ba96f12f2fbf74d799bc22efc7b445
Kevin Ochieng <kevincliffo@gmail.com>
2021-03-26 23:34:51 +0300

    linked images and css

--- app/urls.py
+++ app/urls.py
@@ -5,11 +5,11 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('/aboutus', views.about, name="about-us"),
-    path('/mycareplan', views.mycareplan, name="my-care-plan"),
-    path('/mycareteam', views.mycareteam, name="my-care-team"),
-    path('/contact', views.contact, name="contact"),
-    path('/departments', views.departments, name="departments"),
-    path('/doctors', views.doctors, name="doctors"),
-    path('/blogs', views.blogs, name="blogs"),
+    path('about-us/', views.about, name="about-us"),
+    path('my-care-plan/', views.mycareplan, name="my-care-plan"),
+    path('my-care-team/', views.mycareteam, name="my-care-team"),
+    path('contact/', views.contact, name="contact"),
+    path('departments/', views.departments, name="departments"),
+    path('doctors/', views.doctors, name="doctors"),
+    path('blogs/', views.blogs, name="blogs"),
 ]
